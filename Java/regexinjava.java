ab1a9893373002f92270e69c91e5ab2342acf412
ranveer5289 <ranveer.raghu@gmail.com>
2012-03-25 05:21:24 +0530

    Regex example

---
+++ Java/regexinjava.java
@@ -0,0 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class regexinjava {
+
+        public static void main(String[] args) {
+                Pattern p = Pattern.compile("\\$(\\w.*?)\\$");
+                String s = "Hello $everybody$. How $are$ you $all$";
+
+                Matcher m = p.matcher(s);
+                while (m.find()) {
+                        System.out.println(m.group(1));
+                }
+        }
+
+}
+
