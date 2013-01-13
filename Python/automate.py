41323d12ebbcff132e150e8014d5aae81e76a8af
ranveer <you@example.com>
2013-01-13 16:31:52 +0530

    automate chrome using selenium

---
+++ Python/automate.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from pyvirtualdisplay import Display
+import os
+import time
+
+chromedriver = "/usr/bin/chromedriver"
+os.environ['webdriver.chrome.driver'] = chromedriver
+
+display = Display(visible=0, size=(800,600))
+display.start()
+
+br = webdriver.Chrome(chromedriver)
+br.get("http://www.google.com/search?q=python")
+
+#q = br.find_element_by_name('q')
+#q.send_keys('python')
+#q.send_keys(Keys.RETURN)
+#time.sleep(3)
+print br.title
+
+results = br.find_elements_by_class_name('g')
+#print results
+
+for result in results:
+    print result.text
+    print "-"*140
+
+br.quit()
+display.stop()
